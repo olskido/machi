8a3fc8c7d6cd5d5253bb241fc8ab479c95c14cdd
Olskido <your-email@example.com>
2026-03-04 16:29:14 +0100

    Fix image paths for pumpfun/dexscreener and add machi logo

--- src/components/Community/Community.jsx
+++ src/components/Community/Community.jsx
@@ -6,12 +6,12 @@ const CA = 'ATBR4i19gcQ31Rfr7ymA2XvkCQEAkNFGBtVKTmdqpump'
 const communityLinks = [
   { label: '$MACHI on Pump.fun', href: `https://pump.fun/coin/${CA}`, icon: <img src="/images/pumpfun.png" alt="Pump.fun" style={{ width: '1.2em', height: '1.2em' }} /> },
   { label: 'X Community (907+ Members)', href: 'https://x.com/i/communities/2028397843573018820', icon: '𝕏' },
-  { label: '@machibigbrother', href: 'https://x.com/machibigbrother', icon: '👤' },
+  { label: '@machibigbrother', href: 'https://x.com/machibigbrother', icon: <img src="/images/machi-logo.png" alt="Machi" style={{ width: '1.2em', height: '1.2em', borderRadius: '50%' }} /> },
   { label: 'Chart on DexScreener', href: `https://dexscreener.com/solana/${CA}`, icon: <img src="/images/dexscreener.png" alt="DexScreener" style={{ width: '1.2em', height: '1.2em' }} /> },
 ]
 
 const socials = [
-  { label: 'X / Twitter', href: 'https://x.com/machibigbrother', icon: '𝕏' },
+  { label: 'X', href: 'https://x.com/machibigbrother', icon: '𝕏' },
   { label: 'Community', href: 'https://x.com/i/communities/2028397843573018820', icon: '👥' },
   { label: 'Pump.fun', href: `https://pump.fun/coin/${CA}`, icon: <img src="/images/pumpfun.png" alt="Pump.fun" style={{ width: '1.2em', height: '1.2em' }} /> },
 ]
